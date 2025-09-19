f19e6cca85597daa2ff73ab1804706264312cd3d
Shepilov Vladislav <shepilov.v@protonmail.com>
2025-09-19 18:37:50 +0300

    fix(typing): make typing not failable

--- fast_healthchecks/checks/types.py
+++ fast_healthchecks/checks/types.py
@@ -1,17 +1,49 @@
 """Module containing all the health checks."""
 
-from typing import TypeAlias
+from typing import Any, TypeAlias
 
 from fast_healthchecks.checks._base import HealthCheck, HealthCheckDSN
-from fast_healthchecks.checks.function import FunctionHealthCheck
-from fast_healthchecks.checks.kafka import KafkaHealthCheck
-from fast_healthchecks.checks.mongo import MongoHealthCheck
-from fast_healthchecks.checks.opensearch import OpenSearchHealthCheck
-from fast_healthchecks.checks.postgresql.asyncpg import PostgreSQLAsyncPGHealthCheck
-from fast_healthchecks.checks.postgresql.psycopg import PostgreSQLPsycopgHealthCheck
-from fast_healthchecks.checks.rabbitmq import RabbitMQHealthCheck
-from fast_healthchecks.checks.redis import RedisHealthCheck
-from fast_healthchecks.checks.url import UrlHealthCheck
+
+try:
+    from fast_healthchecks.checks.function import FunctionHealthCheck
+except ImportError:
+    FunctionHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.kafka import KafkaHealthCheck
+except ImportError:
+    KafkaHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.mongo import MongoHealthCheck
+except ImportError:
+    MongoHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.opensearch import OpenSearchHealthCheck
+except ImportError:
+    OpenSearchHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.postgresql.asyncpg import PostgreSQLAsyncPGHealthCheck
+except ImportError:
+    PostgreSQLAsyncPGHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.postgresql.psycopg import PostgreSQLPsycopgHealthCheck
+except ImportError:
+    PostgreSQLPsycopgHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.rabbitmq import RabbitMQHealthCheck
+except ImportError:
+    RabbitMQHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.rabbitmq import RabbitMQHealthCheck
+except ImportError:
+    RabbitMQHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.redis import RedisHealthCheck
+except ImportError:
+    RedisHealthCheck = Any  # ty: ignore[invalid-assignment]
+try:
+    from fast_healthchecks.checks.url import UrlHealthCheck
+except ImportError:
+    UrlHealthCheck = Any  # ty: ignore[invalid-assignment]
 
 Check: TypeAlias = (
     FunctionHealthCheck
